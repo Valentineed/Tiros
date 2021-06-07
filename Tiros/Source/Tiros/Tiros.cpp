bf8f2c13be485a40527259dea1b2a15e0ab5edc6
Axel Ingouf <axel.ingouf@gmail.com>
2021-06-07 15:10:09 -0400

    Unreal project setup.

---
+++ Tiros/Source/Tiros/Tiros.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tiros.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tiros, "Tiros" );
+ 
---
+++ Tiros/Source/Tiros/TirosGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TirosGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATirosGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATirosGameMode();
+};
+
+
+
